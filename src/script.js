4200de566979350d722558f8221e74ae5dfbed67
Lucas.Freitas <lucas.freitas@brasmeg.com.br>
2024-12-05 13:15:44 -0300

    made the list of emojis that are the figures for memorising and made a randow list from this initial list, from it made a for function to use the random list to place the items on the screen, made some css animations for the rotation of the card and added a 'user-select: none;' declaration on the html body to avoid the user to stell information using the browser's tools

--- src/script.js
+++ src/script.js
@@ -1,2 +1,38 @@
-const emojis = []
+const emojis = [
+    "🍕",
+    "🍕",
+    "🍣",
+    "🍣",
+    "🍪",
+    "🍪",
+    "🍧",
+    "🍧",
+    "🍖",
+    "🍖",
+    "🧇",
+    "🧇",
+    "🥓",
+    "🥓",
+    "🥚",
+    "🥚"
+]
 let openCards = []
+
+
+// se random for maior que 0.5 = 2 caso não = -1, dessa forma os itens
+// random da numeros entre 0 e 1, quando o numero for acima de 0.5 sera
+// jogado para a frente da lista, se for abaixo sera jogado para tras
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1))
+
+for(let i=0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item"
+    box.innerHTML = shuffleEmojis[i]
+    box.onclick = handleClick()
+    document.querySelector(".game").appendChild(box)
+}
+
+
+function handleClick () {
+    
+}
